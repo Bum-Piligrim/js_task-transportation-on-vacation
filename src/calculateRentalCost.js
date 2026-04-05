a8ad56786f6160bc3ed41564026500a2a0d14e4f
Oleksandr Kostiushko <a.v.bum25@gmail.com>
2026-04-05 17:44:53 +0300

    add named constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,11 +4,12 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const RENTAL_COST_PER_DAY = 40;
   const LONG_TERM = 7;
   const SEMI_LONG_TERM = 3;
   const LONG_TERM_DISCOUNT = 50;
   const SEMI_LONG_TERM_DISCOUNT = 20;
-  const basePrice = 40 * days;
+  const basePrice = RENTAL_COST_PER_DAY * days;
 
   if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
